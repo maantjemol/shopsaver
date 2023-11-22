f0b59ca679c41e18234396327255e66033585e23
talhac1k <149575887+talhac1k@users.noreply.github.com>
2023-11-22 10:29:20 +0100

    feat: datafetcher

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import { Searchbar } from './components/Searchbar'
 import { SearchResultsList } from './components/SearchresultsList'
-
+import {DataFetcher} from './components/DataFetcher';
 
 function App() {
   const[results, setResults] = useState([])
